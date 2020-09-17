f25947c6ab99d0237fa31bc2601b1c509fc7dd30
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-17 08:44:30 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(res => res.json())
+        .then( (people) => {
+            for (const person in people) {
+                console.log(person.name);
+                }
+            }
+
+        })
+
+    
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default App
